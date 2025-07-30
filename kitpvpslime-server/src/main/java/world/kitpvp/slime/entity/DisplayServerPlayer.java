2eebaf631eac91e8fc2d4844b4d7746bdc6c103c
notstevy <notstevy@ultrabuildmc.de>
2025-07-30 23:57:23 +0200

    Fix respawn finding location
    Signed-off-by: notstevy <notstevy@ultrabuildmc.de>

--- kitpvpslime-server/src/main/java/world/kitpvp/slime/entity/DisplayServerPlayer.java
+++ kitpvpslime-server/src/main/java/world/kitpvp/slime/entity/DisplayServerPlayer.java
@@ -6,7 +6,10 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.portal.TeleportTransition;
+import net.minecraft.world.phys.Vec3;
 import org.bukkit.event.entity.EntityPotionEffectEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.jspecify.annotations.NullMarked;
 
 import javax.annotation.Nullable;
@@ -26,7 +29,47 @@ public final class DisplayServerPlayer extends ServerPlayer {
         return this.displayPlayer.addEffect(effectInstance, entity, cause, fireEvent);
     }
 
+    @Override
+    public TeleportTransition findRespawnPositionAndUseSpawnBlock(boolean useCharge, TeleportTransition.PostTeleportTransition postTeleportTransition, @org.jetbrains.annotations.Nullable PlayerRespawnEvent.RespawnReason respawnReason) {
+        TeleportTransition teleportTransition = new TeleportTransition(this.level(), this.position(), Vec3.ZERO, this.getYRot(), this.getXRot(), (e) -> {});
+        if(respawnReason == null) {
+            return teleportTransition;
+        }
 
+        org.bukkit.entity.Player respawnPlayer = this.getBukkitEntity();
+        org.bukkit.Location location = org.bukkit.craftbukkit.util.CraftLocation.toBukkit(
+            teleportTransition.position(),
+            teleportTransition.newLevel().getWorld(),
+            teleportTransition.yRot(),
+            teleportTransition.xRot()
+        );
+
+        org.bukkit.event.player.PlayerRespawnEvent respawnEvent = new org.bukkit.event.player.PlayerRespawnEvent(
+            respawnPlayer,
+            location,
+            false,
+            false,
+            teleportTransition.missingRespawnBlock(),
+            respawnReason
+        );
+        // Paper end - respawn flags
+        this.level().getCraftServer().getPluginManager().callEvent(respawnEvent);
+
+        location = respawnEvent.getRespawnLocation();
+
+        return new TeleportTransition(
+            ((org.bukkit.craftbukkit.CraftWorld) location.getWorld()).getHandle(),
+            org.bukkit.craftbukkit.util.CraftLocation.toVec3(location),
+            teleportTransition.deltaMovement(),
+            location.getYaw(),
+            location.getPitch(),
+            teleportTransition.missingRespawnBlock(),
+            teleportTransition.asPassenger(),
+            teleportTransition.relatives(),
+            teleportTransition.postTeleportTransition(),
+            teleportTransition.cause()
+        );
+    }
 
     public DisplayPlayer getDisplayPlayer() {
         return displayPlayer;
